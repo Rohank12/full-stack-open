c426450c4ca18ea6b0a429d951f4794ae5223e09
Rohank12 <rohan_kelley@uri.edu>
2024-02-25 18:39:24 -0500

    2.12 - 2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,67 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const Filter = ({ filter, setFilter }) => {
-
-  const handleFilter = (event) => {
-    setFilter(event.target.value)
-  }
-
-  return (
-    <div>
-      filter shown with <input id='name' value={filter} onChange={handleFilter}/>
-    </div>
-  )
-}
-
-const PersonForm = (prop) => {
-
-  const handleNameChange = (event) => {
-    prop.setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    prop.setNewNumber(event.target.value)
-  }
-
-  const addName = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name: prop.newName,
-      number: prop.newNumber
-    }
-    const nameToAdd = prop.persons.findIndex(person => person.name === prop.newName)
-
-    if (nameToAdd !== -1) {
-      alert(`${prop.newName} is already added to phonebook`)
-    } else {
-      prop.setPersons(prop.persons.concat(personObject))
-      prop.setNewName('')
-      prop.setNewNumber('')
-    }
-  }
-
-  return (
-    <form onSubmit={addName}>
-        <div>name: <input value={prop.newName} onChange={handleNameChange}/></div>
-        <div>number: <input value={prop.newNumber} onChange={handleNumberChange}/></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>)
-}
-
-const Persons = ({ persons, filter }) => {
-  const showNames = filter
-  ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-  : persons
-  return (
-    <ul>
-      {showNames.map((person, i) =>
-        <li key={i}>{person.name} {person.number}</li>)}
-    </ul>
-  )
-}
+import personService from './services/persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -70,23 +11,29 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   const hook = () => {
-    axios.get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
+    personService.get().then(initialPersons => {
+      setPersons(initialPersons)
     })
   }
-
   useEffect(hook, [])
+
+  const deleteAction = (personToDelete) => {
+    const confirm = window.confirm(`Delete ${personToDelete.name}?`)
+    if (confirm) {
+      personService.deletePerson(personToDelete.id).then(() => {
+        setPersons(persons.filter(person => person.id !== personToDelete.id))
+      })
+    }
+  }
 
   return (
     <div>
-      debug: {filter}
       <h2>Phonebook</h2>
       <Filter filter={filter} setFilter={setFilter} />
       <h2>Add a new</h2>
       <PersonForm persons={persons} setPersons={setPersons} newName={newName} newNumber={newNumber} setNewName={setNewName} setNewNumber={setNewNumber}/>
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} />
+      <Persons persons={persons} filter={filter} deleteAction={deleteAction} />
     </div>
   )
 }
